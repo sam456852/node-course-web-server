70855be79d1ccb93b488c24c1a6056d3894a3fe2
sam456852 <weixiangzhang@u.northwestern.edu>
2017-05-08 03:42:57 -0500

    Add project page

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use((req,res, next) => {
   next();
 });
 
-app.use((req, res, next) =>{
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) =>{
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -46,6 +46,12 @@ app.get('/bad', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Projects',
   });
 });
 
